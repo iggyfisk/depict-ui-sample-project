050be4fb666931f9aabeadf9312b4df6945d7c5d
Daniel Troger <daniel@natur-kultur.eu>
2023-01-26 13:41:31 +0000

    comments + files I forgot to add

--- src/product_card_template.tsx
+++ src/product_card_template.tsx
@@ -2,6 +2,9 @@ import { ContainedPlaceholderImage, Display, Placeholder } from "@depict-ai/util
 
 const img_aspect_ratio = 2 / 3;
 
+// the configuration (see jsx.d.ts and babel.config.json) and jsx-runtime allows us to use JSX to create normal DOM nodes
+// this is faster than parsing HTML strings and makes it much easier to template things
+// It also allows creating components nicely and makes it easy to use observers and add event listeners to the created nodes (you can just save them in a variable and then add stuff to them, or use use_element or use_listener)
 export function product_card_template(display: Display | null) {
   return [
     <a href={display?.page_url || "javascript:void(0)"} class="product-card">
